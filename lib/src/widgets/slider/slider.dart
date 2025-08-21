05d3b6db780c578916e97275257f9cf9ca2917bc
eliel-hernandez <eliel.hernandez4@upr.edu>
2025-08-21 14:57:06 -0400

    Dart 3 compatibility v2

--- lib/src/widgets/slider/slider.dart
+++ lib/src/widgets/slider/slider.dart
@@ -100,7 +100,7 @@ class OneUISlider extends StatefulWidget {
   /// It is used to display the value of a discrete slider, and it is displayed
   /// as part of the value indicator shape.
   ///
-  /// The label is rendered using the active [ThemeData]'s [TextTheme.bodyText1]
+  /// The label is rendered using the active [ThemeData]'s [TextTheme.bodyLarge]
   /// text style, with the theme data's [ColorScheme.onPrimary] color. The
   /// label's text style can be overridden with
   /// [SliderThemeData.valueIndicatorTextStyle].
